f16ef361fcc73857296074055a41bd41f1614301
YATE ASSEKE RONALD OLIVERA <39405940+yateronald@users.noreply.github.com>
2025-09-26 13:26:47 +0000

    upadte

--- backend/server.js
+++ backend/server.js
@@ -25,12 +25,8 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 const corsOptions = {
     origin: [
-        'https://teaching-qnvf.onrender.com',
-        'http://localhost:5173',
-        'http://localhost:5174',
-        'http://localhost:5175',
-        'http://localhost:5177',
-        'http://localhost:3000'
+        'https://learnfrenchwithnatives.com',
+        
     ],
     credentials: true,
     optionsSuccessStatus: 200
